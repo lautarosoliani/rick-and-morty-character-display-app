aaa0a69e471fb7262d12407af45d3b689fd0dc4e
Lautaro <lautarosoliani@hotmail.com>
2024-05-04 10:32:47 -0300

    feat: CON-01 react-query devtools added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function App({ Component, pageProps }: AppProps) {
    const queryClient = new QueryClient()
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
    return (
       <QueryClientProvider client={queryClient}>
          <Component {...pageProps} />
+         {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
       </QueryClientProvider>
    )
 }
