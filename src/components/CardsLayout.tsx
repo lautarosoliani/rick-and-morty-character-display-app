46d3fbdd336bb1036d0bba19607fbc36fe37ba5c
Lautaro <lautarosoliani@hotmail.com>
2024-05-04 12:00:33 -0300

    feat: CON-01 filters added

--- src/components/CardsLayout.tsx
+++ src/components/CardsLayout.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react'
 import { useCharacters } from '../../hooks/useCharacters'
 import CardCharacter from './CardCharacter'
+import Image from 'next/image'
 
 export type Character = {
    id: number
@@ -11,17 +13,99 @@ export type Character = {
 
 function CardsLayout() {
    const { data, error, isLoading } = useCharacters()
+   const [selectedCharacter1, setSelectedCharacter1] = useState<Character | null>(null)
+   const [selectedCharacter2, setSelectedCharacter2] = useState<Character | null>(null)
 
    if (isLoading) return <div>Loading...</div>
    if (error) return <div>Error: {error.message}</div>
    if (!data) return <div>No data available</div>
 
+   const handleCharacterChange = (e: React.ChangeEvent<HTMLSelectElement>, characterNumber: number) => {
+      const characterId = parseInt(e.target.value, 10)
+      const character = data.find((char: Character) => char.id === characterId)
+      if (characterNumber === 1) {
+         setSelectedCharacter1(character)
+      } else {
+         setSelectedCharacter2(character)
+      }
+   }
+
    return (
-      <div className='flex flex-wrap justify-center'>
-         {data.map((character: Character) => (
-            <CardCharacter key={character.id} {...character} />
-         ))}
-      </div>
+      <>
+         <div className='flex flex-wrap justify-center'>
+            {data.map((character: Character) => (
+               <CardCharacter key={character.id} {...character} />
+            ))}
+         </div>
+         <div className='flex p-10 m-8 space-x-10'>
+            <div className='flex flex-col w-full space-y-5'>
+               <h1 className='text-2xl font-bold text-gray-900 text-center mb-2'>Select Character #1</h1>
+               <div className='flex flex-col w-full space-y-5'>
+                  <div className='flex rounded-lg w-full h-96 items-center justify-center'>
+                     <Image
+                        src={
+                           selectedCharacter1
+                              ? selectedCharacter1.image
+                              : 'https://rickandmortyapi.com/api/character/avatar/19.jpeg'
+                        }
+                        alt={selectedCharacter1 ? selectedCharacter1.name : 'Default Name'}
+                        width={400}
+                        height={400}
+                        priority
+                        className='rounded-full'
+                     />
+                  </div>
+                  <select
+                     className='border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-indigo-500 w-full'
+                     onChange={(e) => handleCharacterChange(e, 1)}
+                     defaultValue=''
+                  >
+                     <option value='' disabled>
+                        Select Character #1
+                     </option>{' '}
+                     {data.map((character: Character) => (
+                        <option key={character.id} value={character.id}>
+                           {character.name}
+                        </option>
+                     ))}
+                  </select>
+               </div>
+            </div>
+            <div className='flex flex-col w-full space-y-5'>
+               <h1 className='text-2xl font-bold text-gray-900 text-center mb-2'>Select Character #2</h1>
+               <div className='flex flex-col w-full space-y-5'>
+                  <div className='flex rounded-lg w-full h-96 items-center justify-center'>
+                     <Image
+                        src={
+                           selectedCharacter2
+                              ? selectedCharacter2.image
+                              : 'https://rickandmortyapi.com/api/character/avatar/19.jpeg'
+                        }
+                        alt={selectedCharacter2 ? selectedCharacter2.name : 'Default Name'}
+                        width={400}
+                        height={400}
+                        priority
+                        className='rounded-full'
+                     />
+                  </div>
+                  <select
+                     className='border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-indigo-500'
+                     onChange={(e) => handleCharacterChange(e, 2)}
+                     defaultValue=''
+                  >
+                     <option value='' disabled>
+                        Select Character #2
+                     </option>{' '}
+                     {data.map((character: Character) => (
+                        <option key={character.id} value={character.id}>
+                           {character.name}
+                        </option>
+                     ))}
+                  </select>
+               </div>
+            </div>
+         </div>
+      </>
    )
 }
 
