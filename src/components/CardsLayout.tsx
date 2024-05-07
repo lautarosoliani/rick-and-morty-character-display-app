72c26b076e02738d7425d7567f669cca4c517aaa
Lautaro <lautarosoliani@hotmail.com>
2024-05-07 13:24:59 -0300

    feat: CON-01 integrate useEpisodes hook to display episodes for each character

--- src/components/CardsLayout.tsx
+++ src/components/CardsLayout.tsx
@@ -3,6 +3,7 @@ import { useCharacters } from '../../hooks/useCharacters'
 import CardCharacter from './CardCharacter'
 import Image from 'next/image'
 import { twMerge } from 'tailwind-merge'
+import { useEpisodes } from '../../hooks/useEpisodes'
 
 export type Character = {
    id: number
@@ -10,12 +11,21 @@ export type Character = {
    status: string
    species: string
    image: string
+   episode: string[]
 }
 
 function CardsLayout() {
    const { data, error, isLoading } = useCharacters()
    const [selectedCharacter1, setSelectedCharacter1] = useState<Character | null>(null)
    const [selectedCharacter2, setSelectedCharacter2] = useState<Character | null>(null)
+   const episodesFirstCharacter = useEpisodes(selectedCharacter1 ? selectedCharacter1.episode : [])
+   const episodesSecondCharacter = useEpisodes(selectedCharacter2 ? selectedCharacter2.episode : [])
+
+   const sharedEpisodes = useEpisodes(
+      selectedCharacter1 && selectedCharacter2
+         ? selectedCharacter1.episode.filter((url) => selectedCharacter2.episode.includes(url))
+         : []
+   )
 
    if (isLoading) return <div>Loading...</div>
    if (error) return <div>Error: {error.message}</div>
@@ -113,29 +123,69 @@ function CardsLayout() {
             </div>
          </div>
          <div className='grid grid-cols-3 p-10 m-8 text-white'>
-            <div
-               className={twMerge(
-                  'text-2xl font-bold text-gray-50 text-center mb-2',
-                  selectedCharacter1 === null && 'invisible'
-               )}
-            >
-               Character #1 - Only Episodes
+            <div>
+               <div
+                  className={twMerge(
+                     'text-2xl font-bold text-gray-50 mb-2',
+                     selectedCharacter1 === null && 'invisible'
+                  )}
+               >
+                  Character #1 - Only Episodes
+               </div>
+               <ul>
+                  {episodesFirstCharacter.map((query, index) =>
+                     query.isLoading ? (
+                        <li key={index}>Loading...</li>
+                     ) : query.error ? (
+                        <li key={index}>Error: {query.error.message}</li>
+                     ) : (
+                        <li key={index}>{query.data.name}</li>
+                     )
+                  )}
+               </ul>
             </div>
-            <div
-               className={twMerge(
-                  'text-2xl font-bold text-gray-50 text-center mb-2',
-                  (!selectedCharacter1 || !selectedCharacter2) && 'invisible'
-               )}
-            >
-               Characters #1 & #2 - Shared Episodes
+            <div>
+               <div
+                  className={twMerge(
+                     'text-2xl font-bold text-gray-50 mb-2',
+                     (!selectedCharacter1 || !selectedCharacter2) && 'invisible'
+                  )}
+               >
+                  Characters #1 & #2 - Shared Episodes
+               </div>
+               <ul>
+                  {sharedEpisodes.map((query, index) =>
+                     query.isLoading ? (
+                        <li key={index}>Loading...</li>
+                     ) : query.error ? (
+                        <li key={index}>Error: {query.error.message}</li>
+                     ) : (
+                        <li key={index}>{query.data.name}</li>
+                     )
+                  )}
+               </ul>
             </div>
-            <div
-               className={twMerge(
-                  'text-2xl font-bold text-gray-50 text-center mb-2',
-                  selectedCharacter2 === null && 'invisible'
-               )}
-            >
-               Character #2 - Only Episodes
+
+            <div>
+               <div
+                  className={twMerge(
+                     'text-2xl font-bold text-gray-50 mb-2',
+                     selectedCharacter2 === null && 'invisible'
+                  )}
+               >
+                  Character #2 - Only Episodes
+               </div>
+               <ul>
+                  {episodesSecondCharacter.map((query, index) =>
+                     query.isLoading ? (
+                        <li key={index}>Loading...</li>
+                     ) : query.error ? (
+                        <li key={index}>Error: {query.error.message}</li>
+                     ) : (
+                        <li key={index}>{query.data.name}</li>
+                     )
+                  )}
+               </ul>
             </div>
          </div>
       </>
