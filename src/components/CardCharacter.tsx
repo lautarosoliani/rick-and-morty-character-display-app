bc0e1b93e3d914fcbfa0fa1632eb35570568911d
Lautaro <lautarosoliani@hotmail.com>
2024-05-07 13:39:42 -0300

    feat: CON-01 size diminished for each card

--- src/components/CardCharacter.tsx
+++ src/components/CardCharacter.tsx
@@ -4,7 +4,7 @@ import { Character } from './CardsLayout'
 export default function CardCharacter(props: Character) {
    const { name, status, species, image } = props
    return (
-      <div className='max-w-[18rem] rounded-lg bg-gray-900 text-gray-50 shadow-xl m-4 shrink-0'>
+      <div className='max-w-[13rem] rounded-lg bg-gray-900 text-gray-50 shadow-xl m-4 shrink-0'>
          <div className='relative overflow-hidden bg-cover bg-no-repeat'>
             <Image src={image} alt={name} width={400} height={400} priority />
          </div>
