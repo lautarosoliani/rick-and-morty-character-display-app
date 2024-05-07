87770bf06d0c95c5f1f8ca9a34757d5f0b099fb4
Lautaro <lautarosoliani@hotmail.com>
2024-05-07 12:17:33 -0300

    feat: CON-01 remove button and corresponding logic added; also logic and styles for Episodes display created

--- src/components/CardsLayout.tsx
+++ src/components/CardsLayout.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { useCharacters } from '../../hooks/useCharacters'
 import CardCharacter from './CardCharacter'
 import Image from 'next/image'
+import { twMerge } from 'tailwind-merge'
 
 export type Character = {
    id: number
@@ -58,7 +59,7 @@ function CardsLayout() {
                   <select
                      className='border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-indigo-500 w-full bg-blue-950 text-white'
                      onChange={(e) => handleCharacterChange(e, 1)}
-                     defaultValue=''
+                     value={selectedCharacter1 ? selectedCharacter1.id.toString() : ''}
                   >
                      <option value='' disabled>
                         Select Character #1
@@ -69,6 +70,9 @@ function CardsLayout() {
                         </option>
                      ))}
                   </select>
+                  <button className='text-white' onClick={() => setSelectedCharacter1(null)}>
+                     Remove Selection
+                  </button>
                </div>
             </div>
             <div className='flex flex-col w-full space-y-5'>
@@ -91,7 +95,7 @@ function CardsLayout() {
                   <select
                      className='border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-indigo-500 bg-blue-950 text-white'
                      onChange={(e) => handleCharacterChange(e, 2)}
-                     defaultValue=''
+                     value={selectedCharacter2 ? selectedCharacter2.id.toString() : ''}
                   >
                      <option value='' disabled>
                         Select Character #2
@@ -102,7 +106,36 @@ function CardsLayout() {
                         </option>
                      ))}
                   </select>
+                  <button className='text-white' onClick={() => setSelectedCharacter2(null)}>
+                     Remove Selection
+                  </button>
                </div>
+            </div>
+         </div>
+         <div className='grid grid-cols-3 p-10 m-8 text-white'>
+            <div
+               className={twMerge(
+                  'text-2xl font-bold text-gray-50 text-center mb-2',
+                  selectedCharacter1 === null && 'invisible'
+               )}
+            >
+               Character #1 - Only Episodes
+            </div>
+            <div
+               className={twMerge(
+                  'text-2xl font-bold text-gray-50 text-center mb-2',
+                  (!selectedCharacter1 || !selectedCharacter2) && 'invisible'
+               )}
+            >
+               Characters #1 & #2 - Shared Episodes
+            </div>
+            <div
+               className={twMerge(
+                  'text-2xl font-bold text-gray-50 text-center mb-2',
+                  selectedCharacter2 === null && 'invisible'
+               )}
+            >
+               Character #2 - Only Episodes
             </div>
          </div>
       </>
