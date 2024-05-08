a41b06c8e8bf12e7d816e279ac11d0c2ff09991d
Lautaro <lautarosoliani@hotmail.com>
2024-05-08 03:34:31 -0300

    fix: CON-01 styles modifications for the characters cards

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -4,14 +4,14 @@ import { Character } from './CharactersDashboard'
 export default function CardCharacter(props: Character) {
    const { name, status, species, image } = props
    return (
-      <div className='max-w-[13rem] rounded-lg bg-gray-900 text-gray-50 shadow-xl m-4 shrink-0'>
+      <div className='max-w-[13rem] rounded-lg bg-gray-900 text-gray-50 shadow-xl m-1 shrink-0'>
          <div className='relative overflow-hidden bg-cover bg-no-repeat'>
-            <Image src={image} alt={name} width={400} height={400} priority />
+            <Image src={image} alt={name} width={200} height={200} priority />
          </div>
-         <div className='p-6'>
-            <h1 className='text-2xl font-bold text-gray-50'>{name}</h1>
-            <div>Status: {status}</div>
-            <div>Species: {species}</div>
+         <div className='p-3'>
+            <h1 className='text-md font-bold'>{name}</h1>
+            <div className='text-xs'>Status: {status}</div>
+            <div className='text-xs'>Species: {species}</div>
          </div>
       </div>
    )
