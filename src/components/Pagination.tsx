d13e25fd08e55431518013a0d78b767c37eedacc
Lautaro <lautarosoliani@hotmail.com>
2024-05-08 12:06:23 -0300

    feat: CON-01 Pagination now is able to go to an specific page

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,6 +1,9 @@
+import React, { useState } from 'react'
+
 type PaginationData = {
    info: {
       next: boolean
+      pages: number
    }
 }
 
@@ -11,8 +14,28 @@ type PaginationProps = {
 }
 
 const Pagination: React.FC<PaginationProps> = ({ page, data, setPage }) => {
+   const [inputPage, setInputPage] = useState('')
+
+   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setInputPage(event.target.value)
+   }
+
+   const goToPage = () => {
+      const pageNumber = parseInt(inputPage, 10)
+      if (pageNumber >= 1 && pageNumber <= data.info.pages && pageNumber !== page) {
+         setPage(pageNumber)
+         setInputPage('')
+      }
+   }
+
+   const handleKeyPress = (event: React.KeyboardEvent) => {
+      if (event.key === 'Enter') {
+         goToPage()
+      }
+   }
+
    return (
-      <div className='flex justify-center mt-4'>
+      <div className='flex justify-center mt-6'>
          <button
             className='bg-blue-950 text-white border-2 border-blue-900 hover:bg-blue-800 font-bold py-2 px-4 rounded-l'
             onClick={() => setPage((old) => Math.max(old - 1, 1))}
@@ -20,7 +43,7 @@ const Pagination: React.FC<PaginationProps> = ({ page, data, setPage }) => {
          >
             Previous
          </button>
-         <div className='text-white font-bold py-2 px-4 '>{`Page ${page}`}</div>
+         <div className='text-white font-bold py-2 px-4'>{`Page ${page} of ${data.info.pages}`}</div>
          <button
             className='bg-blue-950 text-white border-2 border-blue-900 hover:bg-blue-800 font-bold py-2 px-4 rounded-r'
             onClick={() => setPage((old) => (data.info.next ? old + 1 : old))}
@@ -28,6 +51,15 @@ const Pagination: React.FC<PaginationProps> = ({ page, data, setPage }) => {
          >
             Next
          </button>
+         <span className='text-white font-bold py-2 px-4'>Go to page:</span>
+         <input
+            type='text'
+            value={inputPage}
+            onChange={handleInputChange}
+            onKeyPress={handleKeyPress}
+            className='text-center w-full bg-blue-950 text-white border-2 border-blue-900 font-bold rounded-xl hover:pointer'
+            style={{ width: '50px' }}
+         />
       </div>
    )
 }
