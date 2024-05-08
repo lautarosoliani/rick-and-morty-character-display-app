cdd29528015b6fc8e51d465e915f44fbcf2fdf85
Lautaro <lautarosoliani@hotmail.com>
2024-05-08 19:18:10 -0300

    fix: CON-02 unneeded functions removed

--- jest.setup.js
+++ jest.setup.js
@@ -1,14 +1,1 @@
 import '@testing-library/jest-dom'
-
-beforeEach(() => {
-   global.fetch = jest.fn(() =>
-      Promise.resolve({
-         ok: true,
-         json: () => Promise.resolve({ results: [{ id: 1, name: 'Rick Sanchez' }] }),
-      })
-   )
-})
-
-afterEach(() => {
-   jest.restoreAllMocks()
-})
